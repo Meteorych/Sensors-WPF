c84e487e3ebb3359f1be858e103ba5ab5c006f67
Ivan Titlov <super.titlov@inbox.ru>
2024-02-24 00:24:32 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sensors_WPF__.NET_03._1_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
